1e660f53d7e451de6f7cf9c210d5a67cffdf96c4
lduverger <duverger.loic@live.fr>
2024-03-11 20:03:17 +0100

    Create Carousel component and remove arrow/index when just one picture in it

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+import leftArrow from '../assets/images/arrowLeft.svg'
+import rightArrow from '../assets/images/arrowRight.svg'
+
+const Carousel = ({pictures}) => {
+
+    const [index, setIndex] = useState(0);
+
+    const picturesNb = pictures.length;
+
+
+    const handleRight = () => {
+        setIndex((prev) => (prev >= picturesNb - 1 ? 0 : prev + 1));
+    }
+
+    const handleLeft = () => {
+        setIndex((prev) => (prev <= 0 ? picturesNb - 1 : prev - 1));
+    }
+
+
+    return (
+        <div className='carousel'>
+        <img className='carousel_picture' src={pictures[index]} alt="Logement" />
+        {
+            picturesNb > 1 && (
+                <>
+                    <img onClick={handleLeft} className='carousel_arrow carousel_left' src={leftArrow} alt="Fleche gauche" />
+                    <img onClick={handleRight} className='carousel_arrow carousel_right' src={rightArrow} alt="Fleche droite" />
+                    <p className='carousel_index'>{index + 1}/{picturesNb}</p>
+                </>
+            )
+        }
+
+    </div>
+    );
+};
+
+export default Carousel;
--- src/pages/Housing.jsx
+++ src/pages/Housing.jsx
@@ -1,38 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import Tags from '../components/Tags';
 import data from '../assets/data/logements.json'
 import Collapse from '../components/Collapse';
-import leftArrow from '../assets/images/arrowLeft.svg'
-import rightArrow from '../assets/images/arrowRight.svg'
+
 import Notation from '../components/Notation';
+import Carousel from '../components/Carousel';
 const Housing = () => {
 
     const location = useLocation();
     const housing = data.find(housingData => housingData.id === location.state.id);
-    const [index, setIndex] = useState(0);
 
     const firstName = housing.host.name.split(' ')[0];
     const lastName = housing.host.name.split(' ')[1];
 
-    const picturesNB = housing.pictures.length;
-
-    const handleRight = () => {
-        setIndex((prev) => (prev >= picturesNB - 1 ? 0 : prev + 1));
-    }
-
-    const handleLeft = () => {
-        setIndex((prev) => (prev <= 0 ? picturesNB - 1 : prev - 1));
-    }
-
     return (
         <>
-            <div className='carousel'>
-                <img className='carousel_picture' src={housing.pictures[index]} alt="Logement" />
-                <img onClick={handleLeft} className='carousel_arrow carousel_left' src={leftArrow} alt="Fleche gauche" />
-                <img onClick={handleRight} className='carousel_arrow carousel_right' src={rightArrow} alt="Fleche droite" />
-                <p className='carousel_index'>{index + 1}/{picturesNB}</p>
-            </div>
+        <Carousel pictures = {housing.pictures}/>
             <section className='housing_content'>
                 <div className='housing_brief'>
                     <h1 className='housing_title'>{housing.title}</h1>
