3c53f64d2bebc7806ae55b7093135318cd4ba581
lduverger <duverger.loic@live.fr>
2024-03-15 19:02:51 +0100

    fix react/no-escaped-entities error

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -26,7 +26,7 @@ const About = () => {
                 className='collapse-about'
                 title="Sécurité"
             >
-                <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hote qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
+                <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l&apos;hote qu&apos;au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
             </Collapse>
         </div>
     );
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,8 +4,8 @@ const NotFound = () => {
     return (
         <div className='pageNotFound'>
             <h1>404</h1>
-            <p>Oups! La page que vous demandez n'existe pas.</p>
-            <NavLink className="pageNotFound_return" to="/Kasa">Retourner sur la page d'accueil</NavLink>
+            <p>Oups! La page que vous demandez n&apos;existe pas.</p>
+            <NavLink className="pageNotFound_return" to="/Kasa">Retourner sur la page d&apos;accueil</NavLink>
         </div>
     );
 };
